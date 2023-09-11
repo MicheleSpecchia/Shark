3e684495f162d1aa58abace845d75412ab1891e0
riccardomarra <riccardo01.marra@edu.unife.it>
2023-09-11 16:33:28 +0200

    Implementazione logica delle recensioni

--- resources/views/components/park-card.blade.php
+++ resources/views/components/park-card.blade.php
@@ -9,15 +9,15 @@
             <h2 class="fs-4 mt-4 fw-bold" style="color: #1a2a6c;">{{$park->location}}</h2>
             <div class="d-flex bottom mb-2">
                 <div class="rating-cover">
-                    @for ($i = 1; $i <= 5; $i++)
-                        @if ($i <= round($park->rating))
-                            <i class="fas fa-star" style="color: #1a2a6c;"></i>
+                    @for ($i = 1; $i <= 5; $i++) @if ($i <=round($park->average_rating))
+                        <i class="fas fa-star" style="color: #1a2a6c;"></i>
                         @else
-                            <i class="far fa-star" style="color: #1a2a6c;"></i>
+                        <i class="far fa-star" style="color: #1a2a6c;"></i>
                         @endif
-                    @endfor
-                    <span class="ml-2 small" style="color: #1a2a6c;">{{ $park->reviews_count ?? '0' }} reviews</span>
+                        @endfor
+                        <span class="ml-2 small" style="color: #1a2a6c;">{{ $park->reviews_count }} reviews</span>
                 </div>
+
             </div>
             <p class="mb-0 fs-5" style="color: #1a2a6c;">Starting from US$69</p>
         </div>
