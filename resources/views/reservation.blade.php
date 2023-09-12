7b25a4f0affd915f10ecd8036de29e45ec4a4423
riccardomarra <riccardo01.marra@edu.unife.it>
2023-09-12 20:13:06 +0200

    Fixing bug view resrvation

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,9 +1,19 @@
 <x-layout>
     @include('partials._navbar')
 
+    <!-- Contenitore principale -->
     <div class="container mt-4">
-        <h4 class="title-header mb-2">Cancella una prenotazione o lascia una recensione :)</h4>
 
+        <!-- Titolo -->
+        <h4 class="title-header mb-2" id="mainTitle">
+            @if(count($reservations->where('review', null)) > 0)
+                Cancella una prenotazione o lascia una recensione :)
+            @else
+                Per il momento, non hai prenotazioni da gestire
+            @endif
+        </h4>
+
+        <!-- Controllo per verificare la presenza di messaggi nella sessione e visualizzarli -->
         @if(session('message'))
         <div class="notification mb-4">
             {{ session('message') }}
@@ -11,46 +21,50 @@
         @endif
 
         <div class="row mt-5">
-            @unless($reservations->isEmpty())
-            @foreach($reservations as $reservation)
-            @if(is_null($reservation->review))
-            <div class="col-12 col-md-4 col-lg-3 mb-5">
-                <div class="card-wrap p-3 custom-card">
-                    <h3 class="fs-4 mt-4 fw-bold card-title">Prenotazione #{{ $reservation->id }}</h3>
-                    <p><strong>Data Inizio:</strong> {{ $reservation->data_inizio }}</p>
-                    <p><strong>Data Fine:</strong> {{ $reservation->data_fine }}</p>
-                    <p><strong>Ora Inizio:</strong> {{ $reservation->start_time }}</p>
-                    <p><strong>Ora Fine:</strong> {{ $reservation->end_time }}</p>
-                    <p><strong>Prezzo:</strong> {{ $reservation->price }}€</p>
-                    <p><strong>Veicolo:</strong> {{ $reservation->veicolo }}</p>
 
-                    @if(\Carbon\Carbon::now()->lt($reservation->data_inizio))
-                    <button class="btn btn-danger delete-reservation" data-id="{{ $reservation->id }}">Cancella</button>
-                    @elseif(\Carbon\Carbon::now()->gt($reservation->data_fine))
-                    <a href="{{ route('reviews.create', ['park' => $reservation->park_id, 'reservation' => $reservation->id]) }}" class="btn btn-success">Rate</a>
-                    @else
-                    <button class="btn btn-secondary" disabled title="Per effettuare una recensione devi attendere la fine del servizio.">Rate</button>
-                    @endif
-                </div>
-            </div>
-            @endif
+            <!-- Ciclo tra tutte le prenotazioni -->
+            @foreach($reservations as $reservation)
+                <!-- Controllo se la prenotazione non ha una recensione -->
+                @if(is_null($reservation->review))
+                    <!-- Visualizzazione della prenotazione -->
+                    <div class="col-12 col-md-4 col-lg-3 mb-5 reservation-card">
+                        <div class="card-wrap p-3 custom-card">
+                            <!-- Dettagli della prenotazione -->
+                            <h3 class="fs-4 mt-4 fw-bold card-title">Prenotazione #{{ $reservation->id }}</h3>
+                            <p><strong>Data Inizio:</strong> {{ $reservation->data_inizio }}</p>
+                            <p><strong>Data Fine:</strong> {{ $reservation->data_fine }}</p>
+                            <p><strong>Ora Inizio:</strong> {{ $reservation->start_time }}</p>
+                            <p><strong>Ora Fine:</strong> {{ $reservation->end_time }}</p>
+                            <p><strong>Prezzo:</strong> {{ $reservation->price }}€</p>
+                            <p><strong>Veicolo:</strong> {{ $reservation->veicolo }}</p>
+
+                            <!-- Controllo sulla data della prenotazione per decidere quali azioni mostrare -->
+                            @if(\Carbon\Carbon::now()->lt($reservation->data_inizio))
+                                <button class="btn btn-danger delete-reservation" data-id="{{ $reservation->id }}">Cancella</button>
+                            @elseif(\Carbon\Carbon::now()->gt($reservation->data_fine))
+                                <a href="{{ route('reviews.create', ['park' => $reservation->park_id, 'reservation' => $reservation->id]) }}" class="btn btn-success">Rate</a>
+                            @else
+                                <button class="btn btn-secondary" disabled title="Per effettuare una recensione devi attendere la fine del servizio.">Rate</button>
+                            @endif
+                        </div>
+                    </div>
+                @endif
             @endforeach
-            @else
-            <p class="mb-4">Non hai ancora effettuato nessuna prenotazione.</p>
-            @endunless
         </div>
     </div>
+
+    <!-- Script JavaScript per gestire la cancellazione delle prenotazioni -->
     <script>
         $(document).ready(function() {
-            // Questo codice verrà eseguito quando il DOM è completamente caricato
+            // Ascolta l'evento click sui pulsanti per cancellare la prenotazione
             $('.delete-reservation').on('click', function(e) {
-                // Previeni il comportamento standard del click sul pulsante (per esempio la sottomissione di un form)
+                // Previeni l'azione predefinita del pulsante
                 e.preventDefault();
 
-                // Ottieni l'ID della prenotazione dal data attribute del pulsante
+                // Ottieni l'ID della prenotazione
                 let reservationId = $(this).data('id');
 
-                // Mostra un popup di SweetAlert2 per chiedere conferma all'utente
+                // Mostra un popup di conferma prima di cancellare la prenotazione
                 Swal.fire({
                     title: 'Sei sicuro?',
                     text: "Vuoi davvero cancellare questa prenotazione?",
@@ -60,31 +74,34 @@
                     cancelButtonText: 'No, annulla',
                     reverseButtons: true
                 }).then((result) => {
-                    // Se l'utente conferma la cancellazione
+                    // Se l'utente ha confermato
                     if (result.isConfirmed) {
-                        // Fai una chiamata AJAX per cancellare la prenotazione
+                        // Fai una richiesta AJAX per cancellare la prenotazione
                         $.ajax({
                             url: `/reservations/${reservationId}`,
                             method: 'DELETE',
                             headers: {
-                                // Aggiungi il token CSRF alla richiesta AJAX per evitare errori di autenticazione CSRF
                                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                             },
                             success: function(response) {
-                                // Se la cancellazione ha avuto successo
+                                // Gestisci la risposta di successo
                                 if (response.status === 'success') {
-                                    // Mostra un popup di conferma
                                     Swal.fire({
                                         icon: 'success',
                                         title: 'Fatto!',
                                         text: 'Prenotazione cancellata con successo!',
                                         confirmButtonText: 'OK'
                                     });
+                                    // Rimuovi la card della prenotazione
+                                    $(`button[data-id="${reservationId}"]`).closest('.reservation-card').remove();
 
-                                    // Rimuovi la card della prenotazione dalla pagina
-                                    $(`button[data-id="${reservationId}"]`).closest('.col-12').remove();
+                                    // Se non ci sono più prenotazioni, aggiorna il titolo
+                                    if ($('.reservation-card').length === 0) {
+                                        $('#mainTitle').text('Per il momento, non hai prenotazioni da gestire');
+                                    }
+
                                 } else {
-                                    // Se c'è stato un errore durante la cancellazione
+                                    // Gestisci un errore nella risposta
                                     Swal.fire({
                                         icon: 'error',
                                         title: 'Errore!',
@@ -94,7 +111,7 @@
                                 }
                             },
                             error: function() {
-                                // Se c'è stato un errore nella chiamata AJAX
+                                // Gestisci un errore nella richiesta AJAX
                                 Swal.fire({
                                     icon: 'error',
                                     title: 'Errore!',
