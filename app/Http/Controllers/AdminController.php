8807eaf70cbc64da3f19ea28e4c39d6c286c39e3
riccardomarra <riccardo01.marra@edu.unife.it>
2023-09-16 19:03:50 +0200

    implementazione parziale admin page (solo la sezione overview)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Park;
 
 class AdminController extends Controller
 {
-       /**
+    /**
      * Create a new controller instance.
      *
      * @return void
@@ -23,6 +27,31 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard');
+        $reservations_count = [];
+        $cumulative_users_count = [];
+        $parks_daily_count = [];
+        $totalUsers = 0;
+
+        for ($i = 6; $i >= 0; $i--) {
+            $day = Carbon::now()->subDays($i);
+
+            // Prenotazioni
+            $reservationCount = Reservation::whereDate('created_at', $day)->count();
+            $reservations_count[] = $reservationCount;
+
+            // Utenti
+            $totalUsers += User::whereDate('created_at', $day)->count();
+            $cumulative_users_count[] = $totalUsers;
+
+            // Parcheggi
+            $parksCount = Park::whereDate('created_at', $day)->count();
+            $parks_daily_count[] = $parksCount;
+        }
+
+        return view('admin.dashboard', [
+            'reservations_count' => $reservations_count,
+            'cumulative_users_count' => $cumulative_users_count,
+            'parks_daily_count' => $parks_daily_count,
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,210 +1,136 @@
+<!-- Dichiarazione DOCTYPE per HTML5 -->
 <!DOCTYPE html>
-<html class="dash-html" lang="en">
+<html lang="it">
 
 <head>
+    <!-- Specifica la codifica dei caratteri per il documento -->
     <meta charset="UTF-8">
+    <!-- Imposta la viewport per renderla responsive su dispositivi mobili -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <!-- Titolo della pagina -->
+    <title>Dashboard Admin</title>
+    <!-- Importazione della libreria Chart.js per creare grafici -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     @vite(['resources/js/app.js'])
-    <title>SHARK</title>
 </head>
 
-<body class="dash-body">
+<body class="dashboard-overview">
 
-    <!-- Sidebar -->
-    <div class="dash-sidebar">
-        <a href="#" class="dash-a logo">
-            <div class="logo-name logo-text"><span>SH</span>ARK</div>
-        </a>
-        <ul class="side-menu">
-            <li class="dash-li"><a class="dash-a" href="#"><i class='dash-bx bx bxs-dashboard'></i>Dashboard</a></li>
-            <li class="dash-li"><a class="dash-a" href="#"><i class='dash-bx bx bx-store-alt'></i>Shop</a></li>
-            <li class="dash-li active"><a class="dash-a" href="#"><i class='dash-bx bx bx-analyse'></i>Analytics</a></li>
-            <li class="dash-li"><a class="dash-a" href="#"><i class='dash-bx bx bx-message-square-dots'></i>Tickets</a></li>
-            <li class="dash-li"><a class="dash-a" href="#"><i class='dash-bx bx bx-group'></i>Users</a></li>
-            <li class="dash-li"><a class="dash-a" href="#"><i class='dash-bx bx bx-cog'></i>Settings</a></li>
-        </ul>
-        <ul class="side-menu">
-            <li class="dash-li">
-                <form class="inline" method="POST" action="/logout">
-                    <div>
-                        @csrf
-                        <button class="logout" type="submit">
-                            <i class="dash-bx bx bx-log-out-circle"></i>
-                            Logout
-                        </button>
-                    </div>
-                </form>
-            </li>
-        </ul>
-    </div>
-    <!-- End of Sidebar -->
+    <!-- Barra di navigazione in alto -->
+    <div class="navbar">Pannello di Amministrazione Shark</div>
 
-    <!-- Main Content -->
-    <div class="dash-content">
-        <!-- Navbar -->
-        <nav>
-            <i class='dash-bx bx bx-menu'></i>
-            <form action="#">
-                <div class="form-input">
-                    <input type="search" placeholder="Search...">
-                    <button class="search-btn" type="submit"><i class='dash-bx bx bx-search'></i></button>
+    <!-- Contenitore principale -->
+    <div class="container">
+        <!-- Barra laterale con link di navigazione -->
+        <div class="sidebar">
+            <!-- Ogni link punta a una sezione specifica o esegue un'azione -->
+            <a href="#overview">Panoramica</a>
+            <a href="#parcheggi">Parcheggi</a>
+            <a href="#prenotazioni">Prenotazioni</a>
+            <a href="#utenti">Utenti</a>
+            <a href="#settings">Impostazioni</a>
+            <a href="#logout" onclick="performLogout()">Logout</a> <!-- Azione di logout -->
+        </div>
+
+        <!-- Contenuto principale -->
+        <div class="main-content">
+            <!-- Titolo della sezione Panoramica -->
+            <h1>Overview e statistiche generali</h1>
+
+            <!-- Contenitore per il grafico delle prenotazioni -->
+            <div class="table-container">
+                <div class="chart-title">
+                    Prenotazioni recenti
                 </div>
-            </form>
-            <input type="checkbox" id="theme-toggle" hidden>
-            <label for="theme-toggle" class="theme-toggle"></label>
-            <a href="#" class="dash-a notif">
-                <i class='dash-bx bx bx-bell'></i>
-                <span class="count">12</span>
-            </a>
-            <a href="#" class="profile">
-                <img src="images/logosolito.png">
-            </a>
-        </nav>
-
-        <!-- End of Navbar -->
-        <div class="dashboard-container">
-            <main>
-
-                <div class="header">
-                    <div class="left">
-                        <h1>Dashboard</h1>
-                        <ul class="breadcrumb">
-                            <li class="dash-li"><a class="dash-a" href="#">
-                                    Analytics
-                                </a></li>
-                            /
-                            <li class="dash-li"><a href="#" class="dash-a active">Shop</a></li>
-                        </ul>
-                    </div>
-                    <a href="#" class="dash-a report">
-                        <i class='dash-bx bx bx-cloud-download'></i>
-                        <span>Download CSV</span>
-                    </a>
+                <div class="chart-container">
+                    <canvas id="reservationsChart"></canvas> <!-- Area per il grafico delle prenotazioni -->
                 </div>
+            </div>
 
-                <!-- Insights -->
-                <ul class="insights">
-                    <li class="dash-li">
-                        <i class='dash-bx bx bx-calendar-check'></i>
-                        <span class="info">
-                            <h3>
-                                1,074
-                            </h3>
-                            <p>Paid Order</p>
-                        </span>
-                    </li>
-                    <li class="dash-li"><i class='dash-bx bx bx-show-alt'></i>
-                        <span class="info">
-                            <h3>
-                                3,944
-                            </h3>
-                            <p>Site Visit</p>
-                        </span>
-                    </li>
-                    <li class="dash-li"><i class='dash-bx bx bx-line-chart'></i>
-                        <span class="info">
-                            <h3>
-                                14,721
-                            </h3>
-                            <p>Searches</p>
-                        </span>
-                    </li>
-                    <li class="dash-li"><i class='dash-bx bx bx-dollar-circle'></i>
-                        <span class="info">
-                            <h3>
-                                $6,742
-                            </h3>
-                            <p>Total Sales</p>
-                        </span>
-                    </li>
-                </ul>
-                <!-- End of Insights -->
-
-                <div class="bottom-data">
-                    <div class="orders">
-                        <div class="header">
-                            <i class='dash-bx bx bx-receipt'></i>
-                            <h3>Recent Orders</h3>
-                            <i class='dash-bx bx bx-filter'></i>
-                            <i class='dash-bx bx bx-search'></i>
-                        </div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>User</th>
-                                    <th>Order Date</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <img src="images/profile-1.jpg">
-                                        <p>John Doe</p>
-                                    </td>
-                                    <td>14-08-2023</td>
-                                    <td><span class="status completed">Completed</span></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img src="images/profile-1.jpg">
-                                        <p>John Doe</p>
-                                    </td>
-                                    <td>14-08-2023</td>
-                                    <td><span class="status pending">Pending</span></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img src="images/profile-1.jpg">
-                                        <p>John Doe</p>
-                                    </td>
-                                    <td>14-08-2023</td>
-                                    <td><span class="status process">Processing</span></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <!-- Reminders -->
-                    <div class="reminders">
-                        <div class="header">
-                            <i class='dash-bx bx bx-note'></i>
-                            <h3>Remiders</h3>
-                            <i class='dash-bx bx bx-filter'></i>
-                            <i class='dash-bx bx bx-plus'></i>
-                        </div>
-                        <ul class="task-list">
-                            <li class="dash-li completed">
-                                <div class="task-title">
-                                    <i class='dash-bx bx bx-check-circle'></i>
-                                    <p>Start Our Meeting</p>
-                                </div>
-                                <i class='dash-bx bx bx-dots-vertical-rounded'></i>
-                            </li>
-                            <li class="dash-li completed">
-                                <div class="task-title">
-                                    <i class='dash-bx bx bx-check-circle'></i>
-                                    <p>Analyse Our Site</p>
-                                </div>
-                                <i class='dash-bx bx bx-dots-vertical-rounded'></i>
-                            </li>
-                            <li class="dash-li not-completed">
-                                <div class="task-title">
-                                    <i class='bx bx bx-x-circle'></i>
-                                    <p>Play Footbal</p>
-                                </div>
-                                <i class='dash-bx bx bx-dots-vertical-rounded'></i>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- End of Reminders-->
+            <!-- Contenitore per il grafico degli utenti -->
+            <div class="table-container">
+                <div class="chart-title">
+                    Andamento iscritti
                 </div>
-            </main>
+                <div class="chart-container">
+                    <canvas id="usersChart"></canvas> <!-- Area per il grafico degli utenti -->
+                </div>
+            </div>
+
+            <!-- Contenitore per il grafico dei parcheggi -->
+            <div class="table-container">
+                <div class="chart-title">
+                    Parcheggi aggiunti di recente
+                </div>
+                <div class="chart-container">
+                    <canvas id="parksChart"></canvas> <!-- Area per il grafico dei parcheggi -->
+                </div>
+            </div>
         </div>
     </div>
 
-    <script src="dashboard.js"></script>
+    <!-- Elemento nascosto che contiene i dati per i grafici -->
+    <div id="chartData" data-reservations="{{ json_encode($reservations_count) }}" data-cumulative-users="{{ json_encode($cumulative_users_count) }}" data-parks="{{ json_encode($parks_daily_count) }}" style="display:none;">
+    </div>
+
+    <!-- Script che genera i grafici utilizzando i dati forniti e la libreria Chart.js -->
+    <script>
+        // Esegue il codice dopo che il documento è completamente caricato
+        document.addEventListener('DOMContentLoaded', function() {
+            // Raccoglie i dati dai dataset dell'elemento e inizializza le variabili
+            const reservationsData = JSON.parse(document.getElementById('chartData').getAttribute('data-reservations'));
+            const cumulativeUsersData = JSON.parse(document.getElementById('chartData').getAttribute('data-cumulative-users'));
+            const parksData = JSON.parse(document.getElementById('chartData').getAttribute('data-parks'));
+
+            // Funzione per generare un grafico
+            const generateChart = (canvasId, data, title, tipo) => {
+                new Chart(document.getElementById(canvasId), {
+                    type: tipo, // Tipo di grafico
+                    data: {
+                        labels: ['7 giorni fa', '6 giorni fa', '5 giorni fa', '4 giorni fa', '3 giorni fa', '2 giorni fa', 'Ieri'],
+                        datasets: [{
+                            label: title,
+                            data: Object.values(data), // Valori dell'oggetto come dati
+                            backgroundColor: '#1a2a6c',
+                            borderColor: '#1a2a6c', // Colore del bordo
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            }
+
+            // Genera i tre grafici con i dati forniti
+            generateChart('reservationsChart', reservationsData, 'Prenotazioni', 'bar');
+            generateChart('usersChart', cumulativeUsersData, 'Utenti', 'line');
+            generateChart('parksChart', parksData, 'Parcheggi', 'line');
+        });
+
+
+        function performLogout() {
+            // Crea un elemento di modulo nascosto
+            var form = document.createElement('form');
+            form.method = 'POST';
+            form.action = '/logout';
+
+            // Aggiungi un campo nascosto per il token CSRF (assicurati di averlo disponibile)
+            var csrfTokenInput = document.createElement('input');
+            csrfTokenInput.type = 'hidden';
+            csrfTokenInput.name = '_token';
+            csrfTokenInput.value = '{{ csrf_token() }}';
+            form.appendChild(csrfTokenInput);
+
+            // Aggiungi il modulo al corpo del documento e invialo
+            document.body.appendChild(form);
+            form.submit();
+        }
+    </script>
 </body>
 
 </html>
